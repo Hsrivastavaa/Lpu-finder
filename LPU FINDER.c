ed3db626435572dfcca77e73960b55ba9582ed74
Harsh Srivastava <42925762+Hsrivastavaa@users.noreply.github.com>
2018-09-03 15:30:22 +0530

    Add files via upload

---
+++ LPU FINDER.c
@@ -0,0 +1,551 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<math.h>
+#include<time.h>
+main()
+
+
+{
+	 int i,l,j,k,key1,key2,key3,key4,m,n;
+		char r[50],s[50],x[50],y[50],a[50],b[50],c[50],d[50];
+        char q[]="JAI RAM TOPPER";
+        
+	
+	
+	printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+	printf("++++++++++++++++++++++++++++++++++++++++\n");
+	
+	printf("\t\t\t                                      +++++++++++++++++++++++++++++++++++++++++++++");
+	printf("\n\t\t\t                                      +++++++++++++++++++++++++++++++++++++++++++++");
+	printf("\n\t\t\t                                      ++                  LOVELY                 ++");
+	printf("\n\t\t\t                                      ++               PROFFESSIONAL             ++");
+	printf("\n\t\t\t                                      ++                UNIVERSITY               ++");
+	printf("\n\t\t\t                                      ++                                         ++");
+	printf("\n\t\t\t                                      +++++++++++++++++++++++++++++++++++++++++++++");
+	printf("\n\t\t\t                                      +++++++++++++++++++++++++++++++++++++++++++++");
+	
+	
+	printf("\n");
+	printf("\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t*********************************************\n");
+    sleep(1);
+	printf("\t\t\t\t\t\t\t\t**\t\t WELCOME\t\t\t   **\t\t\t\t\t\n");
+	printf("\t\t\t\t\t\t\t\t**\t\t\t\t           **\n");
+	sleep(1);
+	printf("\t\t\t\t\t\t\t\t**\t\t   TO   \t\t   \t   **\n");
+	printf("\t\t\t\t\t\t\t\t**\t\t\t\t\t   \t   **\n");
+	sleep(1);
+	printf("\t\t\t\t\t\t\t\t**\t\tMY PROJECT \t\t\t   **\n");;
+	printf("\t\t\t\t\t\t\t\t**\t\t\t\t\t   \t   **\n");
+	sleep(1);
+    /*printf("\t\t\t\t\t\t\t\t**\t      OF ZODIAC SIGN\t\t\t   **\n");
+	printf("\t\t\t\t\t\t\t\t**\t\t\t\t\t   \t   **\n");
+	sleep(1);
+	printf("\t\t\t\t\t\t\t\t**\t\t    &\t\t\t\t   **\n");
+	printf("\t\t\t\t\t\t\t\t**\t\t\t\t\t   \t   **\n");
+	sleep(1);*/
+	printf("\t\t\t\t\t\t\t\t**\t       OF LPU FINDER\t  \t       **\n");
+	printf("\t\t\t\t\t\t\t\t**\t\t\t\t\t   \t   **\n");
+	sleep(1);
+	printf("\t\t\t\t\t\t\t\t*********************************************\n");
+	
+	getch();
+	system("cls");
+	puts("\n\n\n\t\tENTER YOUR USERNAME :-");
+	gets(r);
+	puts("\n\t\tENTER THE PASSWORD");
+	gets(s);
+	l=strlen(s);
+	for(i=0;i<l;i++)
+	{
+		
+	 if(s[i]==q[i])
+	 {  
+	    //while(i=0)
+//		printf("\n\t\tPASSWORD ACCEPTED");
+		printf("\n\n\t\tPRESS ENTER TO CONTINUE");
+	
+		getch();
+		b:
+		system("cls");
+		printf("\n\n\n\t\t1  :- TO KNOW ABOUT DIFFERENT SECTION");
+		printf("\n\t\t2  :- TO EXIT");
+			printf("\n\t\t     enter your choice  :- ");
+	scanf("%d",&key1);
+	switch(key1)
+	{
+		case 1:
+			system("cls");
+			printf("\n\n\n\t\tDIFFERENT SECTIONS ARE :-");
+			printf("\n\t\t1  :- K17BN");
+			printf("\n\t\t2  :- K17GW");
+			printf("\n\t\t3  :- K17GN");
+            printf("\n\t\t4  :- K17RN");
+            printf("\n\t\t5  :- K17PO");
+            printf("\n\t\t6  :- K17TV");
+            printf("\n\t\t7  :- K17TY");
+            printf("\n\t\t8  :- K17YZ");
+            printf("\n\t\t9  :- K17RP");
+            printf("\n\t\t10 :- K17QW");
+            printf("\n\n\n\n\t\t\t\t  4:- TO KNOW ABOUT SECTION INCHARGES ,SECTION MENTOR,etc.");
+            	printf("\n\t\t\t\t  5:- TO GO TO MAIN MENU ");
+			printf("\n\t\t\t\t  6:- TO EXIT PRESS HERE ");
+			
+		//	getch();
+			printf("\n\t\t\t\t   enter your choice :-"); 
+			scanf("%d",&key2);
+			switch(key2)
+			{
+				 case 4:
+				 	system("cls");
+				 	printf("\n\n\n\t\t ENTER THE SECTION NUMBER  :-");
+				 	printf("\n\t\t\t\t   enter :-"); 
+			scanf("%d",&key3);
+			switch(key3)
+			{
+				case 1:
+					system("cls");
+					printf("\n\n\n\t\tSECTION NAME  :- K17BN ");
+					printf("\n\t\tSECTION MENTOR  :- Horald kumar");
+					printf("\n\t\tSECTION HOD  :- Dinesh Gupta");
+					printf("\n\t\tPLACEMENT MENTOR  :- Nandini kaur ");
+					printf("\n\t\tSTUDENT COUNCELLOR  :- Sophie Turner");
+					printf("\n\t\tECELL COORDINATOR  :- Sharad Kumar " );
+					printf("\n\t\tPLACEMENT COORDINATOR  :- Aditya pandey");
+						printf("\n\n\n\n\t\t\t\t  5:- TO GO TO MAIN MENU ");
+			printf("\n\t\t\t\t  6:- TO EXIT PRESS HERE ");
+		//	getch();
+			printf("\n\t\t\t\t   enter your choice :-"); 
+			scanf("%d",&key4);
+			switch(key4)
+			{
+				 
+				 case 5:
+					goto b;
+					case 6:
+							//case 4:
+			system("cls");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			printf("\t\t\t\t\t\t\t\t\t  +THANK YOU+\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			
+		getch();
+		goto b;
+			
+	}
+	case 2:
+					system("cls");
+					printf("\n\n\n\t\tSECTION NAME  :- K17GW");
+					printf("\n\t\tSECTION MENTOR  :- Joseph paul");
+					printf("\n\t\tSECTION HOD  :- Ravi verma");
+					printf("\n\t\tPLACEMENT MENTOR  :- Jai Kumar");
+					printf("\n\t\tSTUDENT COUNCELLOR  :- Nandini kaur");
+					printf("\n\t\tECELL COORDINATOR  :- Catalyen stark");
+					printf("\n\t\tPLACEMENT COORDINATOR  :- Hemanth kumar ");
+						printf("\n\n\n\n\t\t\t\t  5:- TO GO TO MAIN MENU ");
+			printf("\n\t\t\t\t  6:- TO EXIT PRESS HERE ");
+		//	getch();
+			printf("\n\t\t\t\t   enter your choice :-"); 
+			scanf("%d",&key4);
+			switch(key4)
+			{
+				 
+				 case 5:
+					goto b;
+					case 6:
+							//case 4:
+			system("cls");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			printf("\t\t\t\t\t\t\t\t\t  +THANK YOU+\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			
+		getch();
+			goto b;
+	}
+	case 3:
+					system("cls");
+					printf("\n\n\n\t\tSECTION NAME  :- K17GN");
+					printf("\n\t\tSECTION MENTOR  :- Upinder Kumar");
+					printf("\n\t\tSECTION HOD  :- Ravi kumar");
+					printf("\n\t\tPLACEMENT MENTOR  :- Sandeep kumar ");
+					printf("\n\t\tSTUDENT COUNCELLOR  :- Jaspreet kaur ");
+					printf("\n\t\tECELL COORDINATOR  :- Rosy ");
+					printf("\n\t\tPLACEMENT COORDINATOR  :- Surya ");
+						printf("\n\n\n\n\t\t\t\t  5:- TO GO TO MAIN MENU ");
+			printf("\n\t\t\t\t  6:- TO EXIT PRESS HERE ");
+		//	getch();
+			printf("\n\t\t\t\t   enter your choice :-"); 
+			scanf("%d",&key4);
+			switch(key4)
+			{
+				 
+				 case 5:
+					goto b;
+					case 6:
+							//case 4:
+			system("cls");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			printf("\t\t\t\t\t\t\t\t\t  +THANK YOU+\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			//break;
+		getch();
+			goto b;
+	}
+	case 4:
+					system("cls");
+					printf("\n\n\n\t\tSECTION NAME  :- K17RN ");
+					printf("\n\t\tSECTION MENTOR  :- Preetam singh ");
+					printf("\n\t\tSECTION HOD  :- Pradeep ");
+					printf("\n\t\tPLACEMENT MENTOR  :- Suresh rawat ");
+					printf("\n\t\tSTUDENT COUNCELLOR  :- Akhil kumar ");
+					printf("\n\t\tECELL COORDINATOR  :- Vineeth Kumar");
+					printf("\n\t\tPLACEMENT COORDINATOR  :- Ajeet Gupta ");
+						printf("\n\n\n\n\t\t\t\t  5:- TO GO TO MAIN MENU ");
+			printf("\n\t\t\t\t  6:- TO EXIT PRESS HERE ");
+		//	getch();
+			printf("\n\t\t\t\t   enter your choice :-"); 
+			scanf("%d",&key4);
+			switch(key4)
+			{
+				 
+				 case 5:
+					goto b;
+					case 6:
+							//case 4:
+			system("cls");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			printf("\t\t\t\t\t\t\t\t\t  +THANK YOU+\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			//break;
+		getch();
+		goto b;
+			
+	}
+	case 5:
+					system("cls");
+					printf("\n\n\n\t\tSECTION NAME  :- K17PO");
+					printf("\n\t\tSECTION MENTOR  :- Jeevan gupta");
+					printf("\n\t\tSECTION HOD  :- Anil Mishra");
+					printf("\n\t\tPLACEMENT MENTOR  :- Kedhar chopra");
+					printf("\n\t\tSTUDENT COUNCELLOR  :- Ramesh mishra");
+					printf("\n\t\tECELL COORDINATOR  :- Alekh Srivastava");
+					printf("\n\t\tPLACEMENT COORDINATOR  :- Rakesh Roshan");
+						printf("\n\n\n\n\t\t\t\t  5:- TO GO TO MAIN MENU ");
+			printf("\n\t\t\t\t  6:- TO EXIT PRESS HERE ");
+		//	getch();
+			printf("\n\t\t\t\t   enter your choice :-"); 
+			scanf("%d",&key4);
+			switch(key4)
+			{
+				 
+				 case 5:
+					goto b;
+					case 6:
+							//case 4:
+			system("cls");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			printf("\t\t\t\t\t\t\t\t\t  +THANK YOU+\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			//break;
+		getch();
+			goto b;
+	}
+	case 6:
+					system("cls");
+					printf("\n\n\n\t\tSECTION NAME  :- K17TV");
+					printf("\n\t\tSECTION MENTOR  :- Suresh bacchaan");
+					printf("\n\t\tSECTION HOD  :- Arjun Yadav");
+					printf("\n\t\tPLACEMENT MENTOR  :- Allu Arjun ");
+					printf("\n\t\tSTUDENT COUNCELLOR  :- Kedhar Konkiligada");
+					printf("\n\t\tECELL COORDINATOR  :- Hitesh kumar");
+					printf("\n\t\tPLACEMENT COORDINATOR  :- Anil kumar");
+						printf("\n\n\n\n\t\t\t\t  5:- TO GO TO MAIN MENU ");
+			printf("\n\t\t\t\t  6:- TO EXIT PRESS HERE ");
+		//	getch();
+			printf("\n\t\t\t\t   enter your choice :-"); 
+			scanf("%d",&key4);
+			switch(key4)
+			{
+				 
+				 case 5:
+					goto b;
+					case 6:
+							//case 4:
+			system("cls");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			printf("\t\t\t\t\t\t\t\t\t  +THANK YOU+\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			//break;
+		getch();
+			goto b;
+	}
+	case 7:
+					system("cls");
+					printf("\n\n\n\t\tSECTION NAME  :- K17TY ");
+					printf("\n\t\tSECTION MENTOR  :- Ankit paul");
+					printf("\n\t\tSECTION HOD  :- Amit Bannerjee");
+					printf("\n\t\tPLACEMENT MENTOR  :- Harsh srivastava");
+					printf("\n\t\tSTUDENT COUNCELLOR  :- Vidya Balan");
+					printf("\n\t\tECELL COORDINATOR  :- Sandeep Kaur");
+					printf("\n\t\tPLACEMENT COORDINATOR  :- Jaspreet kaur");
+						printf("\n\n\n\n\t\t\t\t  5:- TO GO TO MAIN MENU ");
+			printf("\n\t\t\t\t  6:- TO EXIT PRESS HERE ");
+		//	getch();
+			printf("\n\t\t\t\t   enter your choice :-"); 
+			scanf("%d",&key4);
+			switch(key4)
+			{
+				 
+				 case 5:
+					goto b;
+					case 6:
+							//case 4:
+			system("cls");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			printf("\t\t\t\t\t\t\t\t\t  +THANK YOU+\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			//break;
+		getch();
+			goto b;
+	}
+	case 8:
+					system("cls");
+					printf("\n\n\n\t\tSECTION NAME  :- K17YZ ");
+					printf("\n\t\tSECTION MENTOR  :- Ashok mittal");
+					printf("\n\t\tSECTION HOD  :- Rashmi Mittal");
+					printf("\n\t\tPLACEMENT MENTOR  :- Lovi Raj Mittal");
+					printf("\n\t\tSTUDENT COUNCELLOR  :- Ranbeer Gupta");
+					printf("\n\t\tECELL COORDINATOR  :- Ajay Kumar");
+					printf("\n\t\tPLACEMENT COORDINATOR  :- Sammy");
+						printf("\n\n\n\n\t\t\t\t  5:- TO GO TO MAIN MENU ");
+			printf("\n\t\t\t\t  6:- TO EXIT PRESS HERE ");
+		//	getch();
+			printf("\n\t\t\t\t   enter your choice :-"); 
+			scanf("%d",&key4);
+			switch(key4)
+			{
+				 
+				 case 5:
+					goto b;
+					case 6:
+							//case 4:
+			system("cls");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			printf("\t\t\t\t\t\t\t\t\t  +THANK YOU+\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			//break;
+		getch();
+			goto b;
+	}
+	case 9:
+					system("cls");
+					printf("\n\n\n\t\tSECTION NAME  :- K17RP");
+					printf("\n\t\tSECTION MENTOR  :- Harshal");
+					printf("\n\t\tSECTION HOD  :- Avneesh singh");
+					printf("\n\t\tPLACEMENT MENTOR  :- Praveen Gupta");
+					printf("\n\t\tSTUDENT COUNCELLOR  :- Amrendra pandey");
+					printf("\n\t\tECELL COORDINATOR  :- Nikhil yadav");
+					printf("\n\t\tPLACEMENT COORDINATOR  :- Salman khan");
+						printf("\n\n\n\n\t\t\t\t  5:- TO GO TO MAIN MENU ");
+			printf("\n\t\t\t\t  6:- TO EXIT PRESS HERE ");
+		//	getch();
+			printf("\n\t\t\t\t   enter your choice :-"); 
+			scanf("%d",&key4);
+			switch(key4)
+			{
+				 
+				 case 5:
+					goto b;
+					case 6:
+						
+						
+						
+						
+							//case 4:
+			system("cls");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			printf("\t\t\t\t\t\t\t\t\t  +THANK YOU+\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			//break;
+		getch();
+			goto b;
+	}
+	case 10:
+					system("cls");
+					printf("\n\n\n\t\tSECTION NAME  :- K17QW");
+					printf("\n\t\tSECTION MENTOR  :- Chandan kumar");
+					printf("\n\t\tSECTION HOD  :- Ajay kumar sahu");
+					printf("\n\t\tPLACEMENT MENTOR  :- Upinder kumar");
+					printf("\n\t\tSTUDENT COUNCELLOR  :- Aditi sharma");
+					printf("\n\t\tECELL COORDINATOR  :- Aman Sinha");
+					printf("\n\t\tPLACEMENT COORDINATOR  :- Manoj kumar");
+						printf("\n\n\n\n\t\t\t\t  5:- TO GO TO MAIN MENU ");
+			printf("\n\t\t\t\t  6:- TO EXIT PRESS HERE ");
+		//	getch();
+			printf("\n\t\t\t\t   enter your choice :-"); 
+			scanf("%d",&key4);
+			switch(key4)
+			{
+				 
+				 case 5:
+					goto b;
+					case 6:
+							//case 4:
+			system("cls");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			printf("\t\t\t\t\t\t\t\t\t  +THANK YOU+\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			//break;
+		getch();
+			goto b;
+	}
+
+
+
+
+			}
+			
+				 case 5:
+					goto b;
+					case 6:
+							//case 4:
+			system("cls");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			printf("\t\t\t\t\t\t\t\t\t  +THANK YOU+\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			break;
+		getch();	
+	}
+					case 2:
+							//case 4:
+			system("cls");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\n");
+		
+		
+		
+			printf("\n");
+			printf("\n");
+			printf("\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			printf("\t\t\t\t\t\t\t\t\t  +THANK YOU+\n");
+			printf("\t\t\t\t\t\t\t\t\t  +++++++++++\n");
+			break;
+		getch();	
+	
+		
+    }
+     }
+	 else
+	 {
+		printf("\n\t\tPASSWORD DENIED");
+	//	return(0);
+	//	goto a;
+	 }
+    }
+   
+	
+}
